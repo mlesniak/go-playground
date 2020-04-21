ad4a726af45f9328aef380b3d8af679820ee72c8
Michael Lesniak <mail@mlesniak.com>
2020-04-21 06:15:06 +0200

    Log to file by default

--- cmd/go-demo/main.go
+++ cmd/go-demo/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	// "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	initializeLogging()
+	serve()
+}
+
+func serve() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
@@ -22,4 +29,18 @@ func main() {
 // Message returns a greeting string.
 func Message() string {
 	return "Hello, world"
+}
+
+func initializeLogging() {
+	// On non-local environments, log to a file using json.
+	// (To be done)
+
+	log.SetFormatter(&logrus.JSONFormatter{})
+	const logfile = "main.log.json"
+	file, err := os.OpenFile(logfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic("Unable to create logfile?")
+	}
+
+	log.SetOutput(file)
 }
