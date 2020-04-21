f8a1b4bbc0bb2299edcb13f1b65a03020cfc5938
Michael Lesniak <mail@mlesniak.com>
2020-04-21 05:43:54 +0200

    Formatting

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"net/http"
 
+	// "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
-		log.WithFields(log.Fields{
-			"ip": c.RealIP(),
-			"method": c.Request().Method,
-		}).Info("Request received")
+		log.Info("Demo logging context")
 		return c.String(http.StatusOK, Message())
 	})
+
 	e.Logger.Info(e.Start(":8080"))
 }
 
