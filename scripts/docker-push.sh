65bbfb0fc7d771882fcb82135523ff16d1bfda8a
Michael Lesniak <mail@mlesniak.com>
2020-04-29 08:13:13 +0200

    Remove debugging output

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -e
-set -x
-
 # See https://docs.travis-ci.com/user/docker/#pushing-a-docker-image-to-a-registry
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push mlesniak/go-demo
