9a4fe060d8f2f94f3f09bc2f92ba4447281da968
Michael Lesniak <mail@mlesniak.com>
2020-04-30 06:30:49 +0200

    Push with updated tag instead of latest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Build new container.
 docker build --build-arg COMMIT=`git rev-parse HEAD` -t mlesniak/go-demo .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push mlesniak/go-demo
+docker push mlesniak/go-demo:${TRAVIS_BUILD_ID}
 
 
 # Deploy to kubernetes by setting new image.
@@ -11,9 +11,8 @@ sed -i -e 's|KUBE_CA_CERT|'"${KUBE_CA_CERT}"'|g'       deployments/kube-config.y
 sed -i -e 's|KUBE_ID|'"${KUBE_ID}"'|g'                 deployments/kube-config.yaml
 sed -i -e 's|KUBE_TOKEN|'"${KUBE_TOKEN}"'|g'           deployments/kube-config.yaml
 
-# We currently do not use $TRAVIS_BUILD_ID intentionally.
 docker run --rm \
     -v $(pwd)/deployments/kube-config.yaml:/.kube/config\
     bitnami/kubectl:latest \
-    set image deployment/go-demo go-demo=${DOCKER_USERNAME}/go-demo:latest
+    set image deployment/go-demo go-demo=${DOCKER_USERNAME}/go-demo:${TRAVIS_BUILD_ID}
 
