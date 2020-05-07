39560b77f256416a06cf4ef8ec787912ab970abf
Michael Lesniak <mail@mlesniak.com>
2020-05-07 05:55:43 +0200

    Remove logging from demo

--- pkg/demo/demo.go
+++ pkg/demo/demo.go
@@ -4,11 +4,8 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/mlesniak/go-demo/pkg/authentication"
-	logger "github.com/mlesniak/go-demo/pkg/log"
+	"github.com/mlesniak/go-demo/pkg/errors"
 )
-
-var log = logger.New()
 
 // AddEndpoint adds the endpoint for testing purposes.
 func AddEndpoint(e *echo.Echo) {
@@ -21,16 +18,13 @@ func AddEndpoint(e *echo.Echo) {
 	}
 
 	e.POST("/api", func(c echo.Context) error {
-		log := authentication.AddUser(log, c)
+		// log := authentication.AddUser(log, c)
 
 		var json request
 		err := c.Bind(&json)
 		if err != nil {
-			log.WithField("error", err).Warn("Unable to parse json")
-			// TODO Use error object
-			return c.String(http.StatusBadRequest, "Unable to parse request")
+			return c.JSON(http.StatusBadRequest, errors.NewResponse("Unable to parse request"))
 		}
-		log.WithField("number", json.Number).Info("Request received")
 		resp := response{json.Number + 1}
 		return c.JSON(http.StatusOK, resp)
 	})
