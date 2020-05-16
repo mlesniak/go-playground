5ef50fd71ce4aeef184ef99f459195378955cbae
Michael Lesniak <mail@mlesniak.com>
2020-05-16 21:05:04 +0200

    Add log on version request

--- pkg/version/handler.go
+++ pkg/version/handler.go
@@ -5,11 +5,13 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
 // AddVersionEndpoint adds the version endpoint
 func AddVersionEndpoint(e *echo.Echo) {
 	e.GET("/api/version", func(c echo.Context) error {
+		log.Info().Msg("Version requested")
 		commit := os.Getenv("COMMIT")
 		if commit == "" {
 			commit = "<No COMMIT environment variable set>"
